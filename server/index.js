d25449bee8a85f207e037984918d6ac1e0bd2c73
AntonDobrev <AntonDobrev@local>
2024-05-21 17:29:24 +0300

    add deployment CI description and ECS dockerrun

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const pgClient = new Pool({
 pgClient.on("connect", (client) => {
   console.log("Connected to Postgres");
   client
-    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .query("CREATE TABLE IF NOT EXISTS fibvalues (number INT)")
     .catch((err) => console.error(err));
 });
 
